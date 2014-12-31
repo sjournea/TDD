313e79d57b1bc40462c885a60637df9ffff77c4b
Steve Journeay <sjournea@taralaur.com>
2014-12-31 16:40:28 -0700

    Moved functional_tests.py into tests and added LiveServerTestcase

---
+++ superlists/functional_tests/tests.py
@@ -0,0 +1,54 @@
+"""functional_tests.py """
+import unittest
+import time
+
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # connect to home page
+        self.browser.get( self.live_server_url )
+    
+        # verify browser title
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # verify invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        # type "Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+        # send Enter Key
+        inputbox.send_keys(Keys.ENTER)
+
+        # verify page updates
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        # verify still another text box inviting to add another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # page updates again
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the Test!!')
+        
