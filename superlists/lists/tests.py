0e07e750df8848cba276937f7aae01f67ff59ab6
Steve Journeay <sjournea@taralaur.com>
2014-12-31 14:45:09 -0700

    Redirect after POST

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.views import home_page
-from lists.models import Item
+from views import home_page
+from models import Item
 
 # Create your tests here.
 
@@ -21,6 +21,11 @@ class HomePageTest(TestCase):
         
         self.assertEqual(response.content.decode(), expected_html)
         
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -28,14 +33,23 @@ class HomePageTest(TestCase):
         
         response = home_page(request)
         
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string('home.html',{ 'new_item_text' : request.POST['item_text']})
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual( new_item.text, 'A new list item')
+        
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
 
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertIn( 'Item 1', response.content.decode())
+        self.assertIn( 'Item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
-    def _test_saving_and_retrieving_items(self):
-        first_itme = Item()
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
         
