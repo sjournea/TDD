1fd8b318446c259a428d42ab9d871dcd7e8354a8
Steve Journeay <sjournea@taralaur.com>
2014-12-31 18:12:51 -0700

    More test updates

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -8,8 +8,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(LiveServerTestCase):
+
+    def createTestBrowser(self):
+        return webdriver.Chrome()
+    
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = self.createTestBrowser()
         self.browser.implicitly_wait(3)
         
     def tearDown(self):
@@ -21,7 +25,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # connect to home page
+        # Edith -- connect to home page
         self.browser.get( self.live_server_url )
     
         # verify browser title
@@ -35,10 +39,11 @@ class NewVisitorTest(LiveServerTestCase):
         
         # type "Buy peacock feathers"
         inputbox.send_keys('Buy peacock feathers')
-        # send Enter Key
         inputbox.send_keys(Keys.ENTER)
-
+        
         # verify page updates
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # verify still another text box inviting to add another item
@@ -49,6 +54,33 @@ class NewVisitorTest(LiveServerTestCase):
         # page updates again
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Now a new user comes along -- Francis
+        self.browser.quit()
+        self.browser = self.createTestBrowser()
+        
+        # Francis visits home page - no sign of Ediths list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_id('id_new_item').text
+        self.assertNotIn('Buy peacock feathers',  page_text)
+        self.assertNotIn('make a fly',  page_text)
+        
+        # Francis creates a new list 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        # Francis gets unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+        self.assertNotEqual( francis_list_url, edith_list_url)
+        
+        # Verify Edith's list is not there
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy Milk', page_text)
+
+
 
         self.fail('Finish the Test!!')
         
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -37,15 +37,14 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual( new_item.text, 'A new list item')
         
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
-
+    def test_home_page_redirects_after_POST(self):
         request = HttpRequest()
-        response = home_page(request)
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
         
-        self.assertIn( 'Item 1', response.content.decode())
-        self.assertIn( 'Item 2', response.content.decode())
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -64,4 +63,20 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Item the Second')
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+        
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        
+        self.assertContains( response, 'Item 1')
+        self.assertContains( response, 'Item 2')
+
     
